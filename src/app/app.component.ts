2cfffe4e2b852ac7ef433ecec12a4c63acefbe1b
haji <muhamedhajajii@outlook.com>
2025-01-13 22:26:35 +0200

    dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { HomeComponent } from './pages/blank-layout/components/home/home.component';
+import { AboutComponent } from './pages/blank-layout/components/about/about.component';
+import { ContactComponent } from './pages/blank-layout/components/contact/contact.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'workshop02';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/blank-layout/components/home/home.component';
+import { AboutComponent } from './pages/blank-layout/components/about/about.component';
+import { ContactComponent } from './pages/blank-layout/components/contact/contact.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'about', component: AboutComponent },
+
+  { path: 'contact', component: ContactComponent },
+
+  { path: '**', component: NotFoundComponent },
+];
